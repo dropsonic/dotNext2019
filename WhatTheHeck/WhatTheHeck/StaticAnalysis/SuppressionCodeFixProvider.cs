da02d02435b2c6b68d5dfeb58c2e7150d94d0671
Vladimir Panchenko <vpanchenko@acumatica.com>
2019-10-11 16:49:04 +0300

    Refactoring

--- WhatTheHeck/WhatTheHeck/StaticAnalysis/SuppressionCodeFixProvider.cs
+++ WhatTheHeck/WhatTheHeck/StaticAnalysis/SuppressionCodeFixProvider.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Text;
 
 namespace WhatTheHeck.StaticAnalysis
@@ -20,6 +17,7 @@ namespace WhatTheHeck.StaticAnalysis
 	{
 		static SuppressionCodeFixProvider()
 		{
+			// Регистрируем текущий code fix provider для всех существующих диагностик
 			Type diagnosticsType = typeof(Descriptors);
 			var propertiesInfo = diagnosticsType.GetRuntimeProperties();
 
@@ -34,36 +32,75 @@ namespace WhatTheHeck.StaticAnalysis
 		}
 
 		private static readonly ImmutableArray<string> _fixableDiagnosticIds;
-		public override ImmutableArray<string> FixableDiagnosticIds { get; } =
-			_fixableDiagnosticIds;
+		public override ImmutableArray<string> FixableDiagnosticIds => _fixableDiagnosticIds;
 
-		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		public override Task RegisterCodeFixesAsync(CodeFixContext context)
 		{
+			// Для каждой найденной диагностики добавляем два code fix'а:
 			foreach (var diagnostic in context.Diagnostics)
 			{
+				// suppress диагностики в suppression файле
 				AddSuppressInFile(context, diagnostic);
-				await AddSuppressByCommentAsync(context, diagnostic).ConfigureAwait(false);
+				// и suppress диагностики с помощью комментария
+				AddSuppressByComment(context, diagnostic);
 			}
+
+			return Task.CompletedTask;
 		}
 
 		private void AddSuppressInFile(CodeFixContext context, Diagnostic diagnostic)
 		{
-			context.RegisterCodeFix(new SuppressInFileCodeAction(diagnostic, context.Document), diagnostic);
+			string title = $"Suppress {diagnostic.Id} in suppression file";
+
+			context.RegisterCodeFix(CodeAction.Create(title, async ct =>
+			{
+				// Пытаемся найти suppression-файл внутри проекта
+				var project = context.Document.Project;
+				var suppressionDoc = project.AdditionalDocuments
+					.FirstOrDefault(d => !String.IsNullOrEmpty(d.FilePath)
+					                     && d.FilePath.EndsWith(SuppressionManager.SuppressionFileExtension));
+
+				Solution solution;
+				// Если файла нет, то создаём его
+				if (suppressionDoc == null)
+				{
+					// Есть открытый баг от 2015 года, что Roslyn не проставляет Build Action = AdditionalFiles
+					// https://github.com/dotnet/roslyn/issues/4655
+					solution = project.Solution.AddAdditionalDocument(DocumentId.CreateNewId(project.Id, debugName: "Suppression File"),
+						name: project.Name + SuppressionManager.SuppressionFileExtension,
+						text: diagnostic.Properties[SuppressionManager.PropertyKey]);
+				}
+				// Если файл уже есть, дописываем в конец новую строчку и обновляем solution
+				else
+				{
+					SourceText text = await suppressionDoc.GetTextAsync(ct).ConfigureAwait(false);
+					string newText = text + Environment.NewLine + diagnostic.Properties[SuppressionManager.PropertyKey];
+					solution = project.Solution
+						.WithAdditionalDocumentText(suppressionDoc.Id, SourceText.From(newText));
+				}
+			
+				return solution;
+			}, title), diagnostic);
 		}
 
-		private async Task AddSuppressByCommentAsync(CodeFixContext context, Diagnostic diagnostic)
+		private void AddSuppressByComment(CodeFixContext context, Diagnostic diagnostic)
 		{
 			string title = $"Suppress {diagnostic.Id} with a comment";
-			var root = await context.Document.GetSyntaxRootAsync().ConfigureAwait(false);
-			var token = root.FindToken(context.Span.Start);
 
-			context.RegisterCodeFix(CodeAction.Create(title, ct =>
+			context.RegisterCodeFix(CodeAction.Create(title, async ct =>
 			{
+				// Находим токен, к которому привязан комментарий
+				var root = await context.Document.GetSyntaxRootAsync(ct).ConfigureAwait(false);
+				var token = root.FindToken(context.Span.Start);
+
+				// Создаём suppression-комментарий
 				var suppressionTrivia = SyntaxFactory.Comment(
 					String.Format(SuppressionManager.SuppressionCommentFormat, diagnostic.Id));
-				string text = diagnostic.Properties[SuppressionManager.PropertyKey];
 
+				// Вставляем его перед оригинальным комментарием с F-word
+				string text = diagnostic.Properties[SuppressionManager.PropertyKey];
 				SyntaxTrivia whitespaceTrivia = SyntaxFactory.Whitespace("");
+
 				foreach (var trivia in token.LeadingTrivia)
 				{
 					if (trivia.IsKind(SyntaxKind.WhitespaceTrivia))
@@ -79,59 +116,9 @@ namespace WhatTheHeck.StaticAnalysis
 						.Insert(0, whitespaceTrivia));
 				
 				var newRoot = root.ReplaceToken(token, newToken);
-				return Task.FromResult(context.Document.WithSyntaxRoot(newRoot));
+				return context.Document.WithSyntaxRoot(newRoot);
 
 			}, title), diagnostic);
-		}
-	}
-
-	internal class SuppressInFileCodeAction : CodeAction
-	{
-		private readonly Diagnostic _diagnostic;
-		private readonly Document _document;
-
-		public SuppressInFileCodeAction(Diagnostic diagnostic, Document document)
-		{
-			_diagnostic = diagnostic;
-			_document = document;
-			Title = EquivalenceKey = $"Suppress {diagnostic.Id} in suppression file";
-		}
-
-		public override string Title { get; }
-		public override string EquivalenceKey { get; }
-
-		// Отключаем preview
-		protected override Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(
-				CancellationToken cancellationToken) => Task.FromResult<IEnumerable<CodeActionOperation>>(null);
-
-		protected override Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
-			=> Task.FromResult<Document>(null);
-
-		protected override async Task<Solution> GetChangedSolutionAsync(CancellationToken cancellationToken)
-		{
-			var project = _document.Project;
-			var suppressionDoc = project.AdditionalDocuments
-				.FirstOrDefault(d => !String.IsNullOrEmpty(d.FilePath)
-				                     && d.FilePath.EndsWith(SuppressionManager.SuppressionFileExtension));
-
-			Solution solution;
-			if (suppressionDoc == null)
-			{
-				solution = project.Solution.AddAdditionalDocument(DocumentId.CreateNewId(project.Id, debugName: "Suppression File"),
-					name: project.Name + SuppressionManager.SuppressionFileExtension,
-					text: _diagnostic.Properties[SuppressionManager.PropertyKey]);
-			}
-			else
-			{
-				SourceText text = await suppressionDoc.GetTextAsync(cancellationToken).ConfigureAwait(false);
-				string newText = text + Environment.NewLine + _diagnostic.Properties[SuppressionManager.PropertyKey];
-				solution = project.Solution
-					.RemoveAdditionalDocument(suppressionDoc.Id)
-					.AddAdditionalDocument(suppressionDoc.Id, suppressionDoc.Name, newText,
-						suppressionDoc.Folders, suppressionDoc.FilePath);
-			}
-			
-			return solution;
 		}
 	}
 }
